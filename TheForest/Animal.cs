a6c58f9167ada5e319bc6189fa6f69f4af20f1e1
norre2k <kevinkvillebo@hotmail.com>
2025-04-01 13:14:08 +0200

    Add project files.

---
+++ TheForest/Animal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForest;
+class Animal
+{
+    public int Weight { get; set; }
+    public bool Nocturnal { get; set; }
+
+
+    public Animal(int weight, bool nocturnal)
+    {
+        Weight = weight;
+        Nocturnal = nocturnal;
+    }
+    public virtual void Move(bool day)
+    {
+        Console.WriteLine("Det okända djuret" + (day == true && Nocturnal == false ? " springer omkring" : " sover"));
+    }
+
+
+}
+
+
+class Owl : Animal
+{
+    public int Wingspan { get; set; }
+
+    public Owl(int weight, bool nocturnal, int wingspan) : base(weight, nocturnal)
+    {
+        //Weight = weight;
+        //Nocturnal = nocturnal;
+        Wingspan = wingspan;
+        
+    }
+
+    public override void Move(bool day)
+    {
+        if(Nocturnal && day == false)
+        {
+            Console.WriteLine("Ugglan flyger i skogen, och letar mat.");
+        }
+        else
+        {
+            Console.WriteLine("Ugglan sover i ett träd.");
+        }
+    }
+}
+
+class Horse : Animal
+{
+    public int HayPerDay { get; set; }
+
+    public Horse(int weight, bool nocturnal, int hayPerDay) : base(weight, nocturnal) 
+    {
+        HayPerDay = hayPerDay;
+    }
+
+    public override void Move(bool day)
+    {
+        if(Nocturnal == false && day == true)
+        {
+            Console.WriteLine("Hästen travar runt u skogen och letar efter hö.");
+        }
+        else
+        {
+            Console.WriteLine("Hästen sover sött under en gran.");
+        }
+    }
+}
+
+class Dolphine : Animal
+{
+    public int DistancePerDay { get; set; }
+
+    public Dolphine(int weight, bool nocturnal, int distancePerDay) : base(weight, nocturnal) 
+    {
+        DistancePerDay = distancePerDay;
+    }
+
+    public override void Move(bool day)
+    {
+        if (Nocturnal == false && day == true)
+        {
+            Console.WriteLine("Delfinen simmar i sjön, och letar efter fisk.");
+        }
+        else
+        {
+            Console.WriteLine("Delfinen sover i vattenbrynet.");
+        }
+    }
+}
---
+++ TheForest/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace TheForest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> forest = new List<Animal>();
+
+            forest.Add(new Animal(1234, true));
+            forest.Add(new Owl(7, true, 34));
+            forest.Add(new Horse(700, false, 70));
+            forest.Add(new Dolphine(430, false, 95));
+            forest.Add(new Owl(9, true, 19));
+
+            bool day = true;
+
+            while (true)
+            {
+                foreach (Animal animal in forest)
+                {
+                    Console.WriteLine(animal.GetType().Name + " Vikt: " + animal.Weight + " kg, " + "sömnvanor: " + (animal.Nocturnal ? "Nattdjur" : "dagdjur"));
+                    if(animal is Owl)
+                    {
+                        Console.WriteLine("En Uggla med vingbredden " + ((Owl)animal).Wingspan + " Cm");
+                    }
+                    else if(animal is Horse)
+                    {
+                        Console.WriteLine("En häst som äter " + ((Horse)animal).HayPerDay + " Kg per dag ");
+                    }
+                    else if(animal is Dolphine){
+                        Console.WriteLine("En delfin som simmar " + ((Dolphine)animal).DistancePerDay + "Km per dag" ) ;
+                    }
+
+                    animal.Move(day);
+                    Console.WriteLine("--------------------------------------");
+                }
+
+                string key = Console.ReadKey().Key.ToString();
+                Console.Clear();
+                switch (key)
+                {
+                    case "D":
+                        day = true;
+                        Console.WriteLine("Nu är det dag");
+                        break;
+                    case "N":
+                        day = false;
+                        Console.WriteLine("Nu är det natt");
+                        break;
+                  
+
+                }
+            }
+        }
+    }
+}
